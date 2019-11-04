a8d61cdc7926d7bfa2d9c7e5f6139a671a445ae1
Student <student@eda-bc-01.localdomain>
2019-11-05 11:21:57 +1300

    Change color every 2 seconds

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,14 +5,14 @@ const App = () => {
 
   let array = []
 
-  for (let i = 0; i < 6000; i++) {
+  for (let i = 0; i <1000; i++) {
     array.push(<Pixel />)
   }
 
   return (
     array
   )
-  
+
 }
 
 export default App
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -3,33 +3,90 @@ import React from 'react'
 const randomHexColor = () =>
   `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
+
+
 class Pixel extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             style : {
-                backgroundColor: randomHexColor(),
+                backgroundColor: this.delayColorChange(),
                 width: '1rem',
                 height: '1rem',
             }
         }
     }
 
-    clickHandler = evt => {
+
+
+    // clickHandler = evt => {
+    //     this.setState({
+    //         style : {
+    //             backgroundColor: randomHexColor(),
+    //             width: '1rem',
+    //             height: '1rem',
+    //         }
+
+    //     })
+    // }
+    // greenify = evt => {
+    //     this.setState({
+    //         style : {
+    //             backgroundColor: '#aaf0d1',
+    //             width: '1rem',
+    //             height: '1rem',
+    //         }
+    //     })
+    // }
+    // makeBlack = evt => {
+    //     evt.preventDefault()
+    //     this.setState({
+    //         style : {
+    //             backgroundColor: 'black',
+    //             width: '1rem',
+    //             height: '1rem',
+    //         }
+    //     })
+    // }
+    makeWhite = evt => {
+        evt.preventDefault()
+        this.setState({
+            style : {
+                backgroundColor: 'white',
+                width: '1rem',
+                height: '1rem',
+            }
+        })
+    }
+    makeYellow = evt => {
+        evt.preventDefault()
+        this.setState({
+            style : {
+                backgroundColor: 'yellow',
+                width: '1rem',
+                height: '1rem',
+            }
+        })
+    }
+
+    delayColorChange = evt => {
+        setInterval(this.colorChanger, 2000)
+    }
+
+    colorChanger = evt => {
         this.setState({
             style : {
                 backgroundColor: randomHexColor(),
                 width: '1rem',
                 height: '1rem',
             }
-
         })
     }
     
     render() {
 
         return (
-            <div style={this.state.style} onClick={this.clickHandler}></div>
+            <div style={this.state.style} onDoubleClick={this.makeWhite} onDragEnter={this.makeYellow}></div>
         )
     }
 }

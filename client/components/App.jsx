134fc14e94a7b19d69655473d2f058e4b9933f13
student <student@eda-bc-03.localdomain>
2019-11-05 11:36:51 +1300

    add working content

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Pixel from './Pixel'
+
+
+function buildArray (content, numberOfItems) {
+  let array = []
+  for (let i=0; i<numberOfItems; i++) {
+    array.push(content)
+  }
+  return array
+}
 
 const App = () => {
   return (
-    <div>React development has begun!</div>
+  // buildArray((<Pixel/>), 250000)
+  Array.from({length: 1000}, (v, i) => <Pixel/>)
   )
 }
 
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+
+const randomHexColor = () =>
+`#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+
+export default class Pixel extends React.Component {
+    constructor(props) {
+        super(props)
+
+
+        this.state = {
+                backgroundColor: 'black'
+    
+        }
+    }
+
+
+    handleEvent = () => {
+        this.setState({
+            backgroundColor: randomHexColor()
+        })
+    }
+
+    turnBlack = () =>{
+        
+        setInterval(() =>{
+            console.log(this)
+            this.setState({
+                backgroundColor: 'black'
+            })
+        }, 1000)
+    }
+   
+    render() {
+        return (
+            <div onClick={this.handleEvent} onMouseOver={this.handleEvent} onMouseLeave={this.turnBlack} style={{
+                height: '50px',
+                width: '50px',
+                backgroundColor: this.state.backgroundColor,
+            }}></div>
+        )
+    }
+
+}

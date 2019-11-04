2d34f5ad44d947d26ee3e6fb2181262d7a4e85d7
Student <student@eda-bc-01.localdomain>
2019-11-05 10:08:57 +1300

    Add pixels

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+
+        return (
+            <div style={{
+                backgroundColor: 'cornflowerblue',
+                width: '6rem',
+                height: '6rem',
+            }}></div>
+        )
+    }
+}
+
+
+export default Pixel

b91d961cc9932330bee8096ba8ab8dd01b9011eb
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-11-26 22:05:33 +1300

    create Pixel component and push it into array

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Pixel from './Pixel'
+
+let arrayOfPixel = []
+
+for(let i=0; i<1000; i++) {
+  arrayOfPixel.push(<Pixel />)
+}
+
+console.log(arrayOfPixel)
 
 const App = () => {
   return (
-    <div>React development has begun!</div>
+    arrayOfPixel
   )
 }
 
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pixel = () => {
+    return (
+        <div style={{width:50, height:50, backgroundColor:'red'}}></div>
+    )
+}
+
+export default Pixel

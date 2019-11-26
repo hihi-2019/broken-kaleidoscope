b56c87e3e4c9065631052b3f221c57c2c7f5e882
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-11-26 22:10:45 +1300

    set initial state with a style property

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
-const Pixel = () => {
-    return (
-        <div style={{width:50, height:50, backgroundColor:'red'}}></div>
-    )
+// const Pixel = () => {
+//     return (
+//         <div style={{width:50, height:50, backgroundColor:'red'}}></div>
+//     )
+// }
+
+class Pixel extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            style : {
+                width:50,
+                height:50,
+                backgroundColor:'red'
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div style={this.state.style}></div>
+        )
+        
+    }
+
+   
 }
 
 export default Pixel
